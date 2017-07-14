e40daa7a21c0e39178b0f549c29eecbf90922a01
masaki <masakiminamide@gmail.com>
2017-07-14 19:35:22 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,14 @@ class UsersController < ApplicationController
   def new
     @user=User.new
   end
+
   def create
     @user = User.new(user_params)
-    if@user.save
-#
+    if @user.save
+      flash[:success]="Welcome to the Sample App!"
+      redirect_to user_url(@user)
     else
+      flash[:danger]="Oops!Something went wrong!"
       render 'new'
     end
   end
@@ -20,6 +23,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
-    
+
 end
 
