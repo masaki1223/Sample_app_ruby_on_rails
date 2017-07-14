765a3ad919d36ca34c5e3a32f13cfb2917fce21e
masaki <masakiminamide@gmail.com>
2017-07-14 17:25:32 +0900

    form

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get '/contact', to:'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to:'users#new'
+  post '/signup', to:'users#create'
+  resources:users
 end
